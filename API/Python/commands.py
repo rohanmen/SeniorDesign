701bda5fc776c6eeff26a0595c7c4a3bf5bb9c59
gonnapwnu <rohanmen@yahoo.com>
2016-03-02 22:55:19 -0600

    updated data

--- API/Python/commands.py
+++ API/Python/commands.py
@@ -11,6 +11,15 @@ import mcp3008
 #define all pins here
 LED = 7
 #servo goes to GPIO 18
+SPICLK = 23
+SPIMISO = 21
+SPIMOSI = 19
+SPICS = 22
+
+LOW_VAL = 0
+PULL_OUT = 1
+ 
+
 
 #***************************************************************
 #******servo control functions**********************************
@@ -36,6 +45,13 @@ def setup():
 	GPIO.setup(LED, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 	#GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	#add setup code here
+
+	# set up the SPI interface pins
+	GPIO.setup(SPIMOSI, GPIO.OUT)
+	GPIO.setup(SPIMISO, GPIO.IN)
+	GPIO.setup(SPICLK, GPIO.OUT)
+	GPIO.setup(SPICS, GPIO.OUT)
+
 
 	#servo setup
 	set("delayed", "0")			#turn the delay mode off
@@ -85,4 +101,79 @@ def move_servo(start_angle, end_angle):
 		for angle in range(end_angle, start_angle):
 			setServo(start_angle - angle)
 			time.sleep(delay_period)
+
+
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+ 
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+ 
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+ 
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+ 
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+def extend_lin_actuator():
+	print "extend"
+	#set bits
+
+def retract_lin_actuator():
+	print "retract"
+	#set bits
+
+def stop_lin_actuator():
+	print "stop"
+	#set bits
+
+def pull_to_zero():
+	print "zero"
+	retract_lin_actuator()
+	i=0
+	while (readadc(0,SPICLK,SPIMOSI,SPIMISO,SPICS) > distance):
+		i++
+	stop_lin_actuator()
+
+
+def pull_psu(distance):
+	print distance
+	i = 0;
+	extend_lin_actuator()
+	while (readadc(0,SPICLK,SPIMOSI,SPIMISO,SPICS) < distance):
+		i++
+
+	wait(5)
+
+	retract_lin_actuator()
+	while (readadc(0,SPICLK,SPIMOSI,SPIMISO,SPICS) > PULL_OUT):
+		i++
+
+	stop_lin_actuator()
+	wait(5)
+
 
--- API/Python/server.py
+++ API/Python/server.py
@@ -1,16 +1,25 @@
 import json
 import time
 import urllib2
-import commands
+#import commands
 import subprocess
 
 bashCommand = "hostname -I"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-IP = process.communicate()[0].split()[0]
+#process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+#IP = process.communicate()[0].split()[0]
+IP =  "172.20.10.3"
 
 PORT = '8080'
 URL = 'http://' + IP + ':' + PORT + '/api/command' 
 print URL
+
+with open("Python/data.json") as json_file:
+    coordinates = json.load(json_file)
+
+
+def pull_psu(distance):
+	print distance
+
 
 while True:
 
@@ -20,8 +29,13 @@ while True:
 
 		if(data['type'] == 'pull_psu'):
 			print 'pull_psu', data['psu_id']
+			index = int(data['psu_id'])
+			pull_psu(coordinates[index]['x'])
+
 		elif(data['type'] == 'push_psu'):
 			print 'push_psu', data['psu_id']
+		elif(data['type'] == 'wait'):
+			print 'waiting', data['id'], 'seconds'
 	except:
 		print 'no connection'
 
