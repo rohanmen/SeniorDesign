746e92b3004dc80b6cd7c9824ffdeab521561f30
gonnapwnu <rohanmen@yahoo.com>
2016-04-05 22:40:58 -0500

    fixed bug

--- API/Python/commands.py
+++ API/Python/commands.py
@@ -373,7 +373,7 @@ def calibrate():
 	wait(0.5)
 	#while (not(GPIO.input(C_BUTTON))):
 		#pass
-	while (get_current_feedback() < 80):
+	while (get_current_feedback() < 90):
 		pass
 
 	stop_lin_actuator()
